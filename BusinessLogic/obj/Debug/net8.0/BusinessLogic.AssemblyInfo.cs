173b6842970963fcd71aaefcb7a0c56a71e90120
Hungnphe186981 <144997703+HungNPHE@users.noreply.github.com>
2025-03-19 15:56:46 +0700

    merge main

--- BusinessLogic/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
+++ BusinessLogic/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1addaba23569233c8f87ea128ee44400e0085707")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c2d0f73b3c2220d05a9a51a34335ac8da2435cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
