3c30e292a71c267737fd03f86dc9984dab9ee0e4
unknown <ahntuan284@gmail.com>
2025-03-20 10:39:45 +0700

    Update feature: Tracking more action of admin

--- BusinessLogic/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
+++ BusinessLogic/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da56eceee4355f73824e5031e34c48ce9b0c9228")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac029e779951057f4ff1c488ce08c78ede93aca")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da56eceee4355f73824e5031e34c48ce9b0c9228")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac029e779951057f4ff1c488ce08c78ede93aca")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
